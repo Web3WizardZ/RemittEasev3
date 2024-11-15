347846f89909f9c0b695f10d05b68afab0b02427
Web3WizardZ <augustusflick@gmail.com>
2024-11-15 14:44:57 +0200

    modified:   src/components/ui/loading.tsx

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -1,8 +1,8 @@
 // src/components/ui/loading.tsx
 export function Loading() {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-black"></div>
-      </div>
-    );
-  }
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-black"></div>
+    </div>
+  );
+}
